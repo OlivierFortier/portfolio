ec55f5584093d91bd7fd0fdf5ae25ae023797c92
Olivier Fortier <olivier_fortier@outlook.com>
2021-02-21 15:50:03 -0500

    menu mobile fonctionnel

--- components/Header.tsx
+++ components/Header.tsx
@@ -19,11 +19,11 @@ export default function Header() {
                     <summary className="relative z-10 list-none text-7xl">
                        <IoMenu/>
                     </summary>
-                    <ul className="absolute top-0 left-0 flex flex-col h-screen w-screen bg-blue-300">
-                        <li ><Link href="/"><a>Accueil</a></Link></li>
-                        <li ><Link href="/"><a>Projets</a></Link></li>
-                        <li ><Link href="/"><a>À propos</a></Link></li>
-                        <li ><Link href="/"><a>Contact</a></Link></li>
+                    <ul className="absolute top-0 left-0 flex flex-col h-screen w-screen bg-blue-300 justify-center items-center space-y-7 text-4xl">
+                        <li><Link href="/"><a>Accueil</a></Link></li>
+                        <li><Link href="/"><a>Projets</a></Link></li>
+                        <li><Link href="/"><a>À propos</a></Link></li>
+                        <li><Link href="/"><a>Contact</a></Link></li>
                     </ul> 
                 </details>
             </nav>
