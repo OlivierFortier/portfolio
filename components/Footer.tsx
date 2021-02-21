e5a5219667ddd8e02a982d30eb7526b370300e0f
Olivier Fortier <olivier_fortier@outlook.com>
2021-02-21 17:02:14 -0500

    padding footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import {FiGithub, FiSend} from 'react-icons/fi';
 
 export default function Footer() {
     return (
-        <footer className="flex flex-col items-center">
+        <footer className="flex flex-col items-center pb-8">
             <hr className="h-1 w-10/12 bg-blue-600"/>
             <nav className=" flex flex-col md:w-full md:flex-row md:justify-around">
                 <h3 className="text-center py-3 text-xl "><Link href="/"><a>Olivier Fortier 2021</a></Link></h3>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,48 @@
 import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-     <h1 className="text-7xl">allo monde</h1>
+    <div className="max-w-xs mx-auto">
+      <section className="mb-4 flex flex-col-reverse md:flex-col">
+        <span className="space-y-2">
+          <h1 className="text-5xl mt-2">Olivier Fortier</h1>
+          <h2 className="text-2xl">
+            Salut, je suis un développeur web passionné par les nouvelles
+            technologies 🤙
+          </h2>
+          <Link href="/">
+            <a className="inline-block p-2 border-4 border-blue-700 rounded-md">
+              En savoir plus 👋
+            </a>
+          </Link>
+        </span>
+        <Image
+          src="/assets/diego_unsplash.jpg"
+          width={445}
+          height={394}
+          alt="hero"
+          className=" object-cover rounded-lg"
+        />
+      </section>
+
+      <section>
+        <p className="bg-blue-200 p-3 rounded-xl">
+          J’adore travailler avec des outils modernes et efficaces. Je passe mon
+          temps à jouer avec des frameworks et des librairies pour le plaisir.
+          Ça m’arrive aussi d’aider les gens , les conseiller et les diriger
+          vers une solution optimale.
+        </p>
+        <p>
+          J’aime réaliser des projets de A à Z, en passant par le DevOps, le
+          Back-End, aussi bien que le Front-End. J’aime avoir une vision global
+          d’un produit, et toucher à tout ce qui à trait au cycle de vie d’un
+          produit multimédia. C’est pourquoi je suis inscrit à L’ETS en Génie
+          Logiciel.
+        </p>
+      </section>
+      
     </div>
   );
 }
