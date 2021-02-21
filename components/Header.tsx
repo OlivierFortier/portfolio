e33ef465407e96060903e9f359c6e98ef6f0bdaa
Olivier Fortier <olivier_fortier@outlook.com>
2021-02-21 15:45:30 -0500

    structure sémantique menu burger

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,15 +16,15 @@ export default function Header() {
                 </ul>
 
                 <details className="list-none md:hidden">
-                    <summary className="list-none text-7xl">
+                    <summary className="relative z-10 list-none text-7xl">
                        <IoMenu/>
                     </summary>
-                    <ul className="absolute flex flex-col">
-                    <li><Link href="/"><a>Accueil</a></Link></li>
-                    <li><Link href="/"><a>Projets</a></Link></li>
-                    <li><Link href="/"><a>À propos</a></Link></li>
-                    <li><Link href="/"><a>Contact</a></Link></li>
-                </ul> 
+                    <ul className="absolute top-0 left-0 flex flex-col h-screen w-screen bg-blue-300">
+                        <li ><Link href="/"><a>Accueil</a></Link></li>
+                        <li ><Link href="/"><a>Projets</a></Link></li>
+                        <li ><Link href="/"><a>À propos</a></Link></li>
+                        <li ><Link href="/"><a>Contact</a></Link></li>
+                    </ul> 
                 </details>
             </nav>
         </header>
