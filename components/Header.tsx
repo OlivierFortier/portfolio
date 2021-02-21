56e7c5b133b58431c801365f6e95a85b8f8db550
Olivier Fortier <olivier_fortier@outlook.com>
2021-02-21 15:37:16 -0500

    structure header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,23 +1,25 @@
 import Link from 'next/link';
+import {SiGravatar} from 'react-icons/si';
+import {IoMenu} from 'react-icons/io5';
 
 export default function Header() {
     return (
         <header id="Header">
-            <nav className="flex justify-between">
-                <span><Link href="/"><a>logo</a></Link></span>
+            <nav className="flex justify-between items-center px-2">
+                <span className="text-5xl"><Link href="/"><a><SiGravatar/></a></Link></span>
 
-                {/* <ul className="flex">
+                <ul className="hidden md:flex">
                     <li><Link href="/"><a>Accueil</a></Link></li>
                     <li><Link href="/"><a>Projets</a></Link></li>
                     <li><Link href="/"><a>À propos</a></Link></li>
                     <li><Link href="/"><a>Contact</a></Link></li>
-                </ul> */}
+                </ul>
 
-                <details className="list-none">
-                    <summary className="list-none">
-                       <i>burger</i>
+                <details className="list-none md:hidden">
+                    <summary className="list-none text-7xl">
+                       <IoMenu/>
                     </summary>
-                    <ul className="flex flex-col">
+                    <ul className="absolute flex flex-col">
                     <li><Link href="/"><a>Accueil</a></Link></li>
                     <li><Link href="/"><a>Projets</a></Link></li>
                     <li><Link href="/"><a>À propos</a></Link></li>
