d1611d7e22a5e5d4484742e65885e0e794ff02dd
Olivier Fortier <olivier_fortier@outlook.com>
2021-02-22 18:04:09 -0500

    ajustement projets

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,7 +9,6 @@ export default function Layout({children}) {
 
       <main>{children}</main>
 
-      {/* <Footer /> */}
       <Footer2/>
       
     </div>
--- components/Projets/Projets1.tsx
+++ components/Projets/Projets1.tsx
@@ -4,7 +4,10 @@ import Projet from "./Projet";
 export default function Projets1() {
   return (
     <section className="text-gray-600 body-font py-8">
-      <h2 className="w-full text-center mb-10 text-4xl">Projets récents</h2>
+      <h2 className="text-center mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+        Projets récents
+      </h2>
+      <p className="text-base text-center text-gray-700 md:text-lg mb-8">Une collection de mes plus récents projets intéressants</p>
       <div className="container px-5  mx-auto flex flex-wrap">
         <div className="flex flex-wrap -m-4">
           <Projet />
