f52309dc3db72ba73d496a8fa60a3da602065f92
Olivier Fortier <olivier_fortier@outlook.com>
2021-02-22 18:15:07 -0500

    structure sémantique liste projet

--- components/Projets/Projets1.tsx
+++ components/Projets/Projets1.tsx
@@ -4,17 +4,17 @@ import Projet from "./Projet";
 export default function Projets1() {
   return (
     <section className="text-gray-600 body-font py-8">
-      <h2 className="text-center mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+      <h1 className="sm:text-center mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
         Projets récents
-      </h2>
-      <p className="text-base text-center text-gray-700 md:text-lg mb-8">Une collection de mes plus récents projets intéressants</p>
+      </h1>
+      <p className="text-base sm:text-center text-gray-700 md:text-lg mb-8">Une collection de mes plus récents projets intéressants</p>
       <div className="container px-5  mx-auto flex flex-wrap">
-        <div className="flex flex-wrap -m-4">
+        <ul className="flex flex-wrap -m-4">
           <Projet />
           <Projet />
           <Projet />
           <Projet />
-        </div>
+        </ul>
       </div>
     </section>
   );
