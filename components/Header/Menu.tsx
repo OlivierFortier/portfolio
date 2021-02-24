a4251915c8f3f95305706295edd08fb24517bc6b
Olivier Fortier <olivier_fortier@outlook.com>
2021-02-23 22:08:56 -0500

    fix tailwind build time

--- components/Header/Menu.tsx
+++ components/Header/Menu.tsx
@@ -6,46 +6,57 @@ export default function Menu() {
   const [menuOuvert, setMenuOuvert] = useState(false);
 
   function basculerMenu() {
-      setMenuOuvert(etatMenu => !etatMenu);
+    setMenuOuvert((etatMenu) => !etatMenu);
   }
 
   useEffect(() => {
-    menuOuvert && ( document.body.style.overflow = "hidden");
+    menuOuvert && (document.body.style.overflow = "hidden");
     !menuOuvert && (document.body.style.overflow = "scroll");
-   }, [menuOuvert]);
+  }, [menuOuvert]);
 
   return (
-    <details id="Menu" className="list-none lg:hidden">
-      <summary
-        onClick={basculerMenu}
-        className="relative z-30 list-none text-7xl"
-      >
-        <IoMenu />
-      </summary>
-      <div className="z-20 fixed top-0 left-0 h-screen w-screen bg-blue-300">
-        <ul className="h-full flex flex-col justify-center items-center space-y-7 text-4xl">
-          <li>
-            <Link href="/">
-              <a>Accueil</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              <a>Projets</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              <a>À propos</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              <a>Contact</a>
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </details>
+    <>
+      <details id="Menu" className="list-none lg:hidden">
+        <summary
+          onClick={basculerMenu}
+          className="relative z-30 list-none text-7xl"
+        >
+          <IoMenu />
+        </summary>
+        <div className="z-20 fixed top-0 left-0 h-screen w-screen bg-blue-300">
+          <ul className="h-full flex flex-col justify-center items-center space-y-7 text-4xl">
+            <li>
+              <Link href="/">
+                <a>Accueil</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a>Projets</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a>À propos</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a>Contact</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </details>
+      {/* <style jsx>{`
+        #Menu > summary {
+          list-style: none;
+        }
+
+        #Menu > summary::-webkit-details-marker {
+          display: none;
+        }
+      `}</style> */}
+    </>
   );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,16 @@
-import 'tailwindcss/tailwind.css';
-import '../styles/globals.css';
+import '../styles/fonts.css';
+
+// import 'tailwindcss/tailwind.css';
+
+import '../styles/tailwind.css';
+import '../styles/utilities.css';
+
+// import "tailwindcss/base.css";
+// import "tailwindcss/components.css";
+// import "tailwindcss/utilities.css";
+
+import '../styles/detailsFix.css';
+
 import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
