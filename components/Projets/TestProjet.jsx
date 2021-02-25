01ee6ffd0173c7fe28698a05383b396564dbb689
Olivier Fortier <olivier_fortier@outlook.com>
2021-02-25 18:31:03 -0500

    ébauche pages de projets spécifique

---
+++ components/Projets/TestProjet.jsx
@@ -0,0 +1,70 @@
+import Link from 'next/link';
+
+export default function TestProjet() {
+  return (
+    <section className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="max-w-xl mb-10   lg:max-w-2xl md:mb-12">
+        <h1 className="max-w-lg mb-6 font-sans text-4xl font-bold leading-none tracking-tight text-blue-600 sm:text-6xl ">
+          Un certain projet
+        </h1>
+        <h2 className="text-base text-blue-500 md:text-lg">
+          Une solution à un problème y.
+        </h2>
+      </div>
+      <div className="grid max-w-screen-xl gap-8 lg:grid-cols-2 sm:mx-auto">
+        <div className="grid grid-cols-2 gap-5">
+          <img
+            className="object-cover w-full h-56 col-span-2 rounded shadow-lg"
+            src="https://images.pexels.com/photos/3182746/pexels-photo-3182746.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+            alt=""
+          />
+          <img
+            className="object-cover w-full h-48 rounded shadow-lg"
+            src="https://images.pexels.com/photos/3184296/pexels-photo-3184296.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+            alt=""
+          />
+          <img
+            className="object-cover w-full h-48 rounded shadow-lg"
+            src="https://images.pexels.com/photos/3184338/pexels-photo-3184338.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+            alt=""
+          />
+        </div>
+        <div className="flex flex-col justify-center">
+          <div className="pb-4 mb-4 border-b">
+            <h3 className="mb-2 font-semibold text-blue-600 leading-5">
+              Un sous-titre
+            </h3>
+            <p className="text-sm text-blue-500">
+              Sportacus andrew weatherall goose Refined gentlemen super mario
+              des lynam alpha trion zap rowsdower, omar sharif old.
+            </p>
+          </div>
+          <div className="pb-4 mb-4 border-b">
+            <h3 className="mb-2 font-semibold text-blue-600 leading-5">
+              Un autre sous-titre
+            </h3>
+            <p className="text-sm text-blue-500">
+              Chase ball of string eat plants, meow, and throw up because I ate
+              plants going to catch the red dot today going to catch the red dot
+              today. I could pee on this if I had the energy.
+            </p>
+          </div>
+          <div>
+            <h3 className="mb-2 font-semibold text-blue-600 leading-5">
+              Stack technologique
+            </h3>
+            <ul className="text-sm text-blue-500">
+              <li className="md:ml-3"> - React</li>
+              <li className="md:ml-3"> - Next.js</li>
+              <li className="md:ml-3"> - TailwindCSS</li>
+              <li className="md:ml-3"> - Typescript</li>
+              <li className="md:ml-3"> - Headless CMS</li>
+            </ul>
+          </div>
+        </div>
+        <Link href="/projets"><a className="place-self-start cursor-pointer font-semibold text-blue-600 p-2 my-6 border-blue-600 border-2 rounded transition-colors hover:bg-blue-600 hover:text-white" > {"<"} Retour aux projets</a></Link>
+      </div>
+      
+    </section>
+  );
+}
---
+++ pages/projets/[slug].jsx
@@ -0,0 +1,8 @@
+import ListeProjets from "../../components/Projets/ListeProjets";
+import TestProjet from "../../components/Projets/TestProjet";
+
+export default function Projet() {
+  return (
+   <TestProjet/>
+  );
+}
