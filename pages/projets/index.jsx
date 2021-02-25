456a545cfe2fb4c6a5f1a6fe138e0e6ec69ac4f9
Olivier Fortier <olivier_fortier@outlook.com>
2021-02-24 23:14:01 -0500

    imports inutiles

--- pages/projets/index.jsx
+++ pages/projets/index.jsx
@@ -1,7 +1,4 @@
-import Hero from "../../components/Hero/Hero";
-import AppercuTechnos from "../../components/Technos/AppercuTechnos";
 import Projets from "../../components/Projets/Projets";
-import Perso from "../../components/Perso/Perso";
 
 export default function index() {
   return (
