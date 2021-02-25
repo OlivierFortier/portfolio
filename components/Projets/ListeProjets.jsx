ecf53215c67743306958746d85a6ba77a22e360b
Olivier Fortier <olivier_fortier@outlook.com>
2021-02-25 11:15:57 -0500

    page projets

--- components/Projets/ListeProjets.jsx
+++ components/Projets/ListeProjets.jsx
@@ -1,7 +1,26 @@
+import Projet from './Projet';
+
 export default function ListeProjets() {
     return (
-        <div>
-            
-        </div>
+        <section className=" text-gray-600 body-font py-8 sm:py-14">
+      <h1 className="px-4 sm:text-left mb-6 font-sans text-4xl font-bold tracking-tight text-blue-600 sm:text-6xl sm:leading-none">
+        Mes projets
+      </h1>
+      <p className="px-4 text-base sm:text-left text-blue-500 md:text-2xl mb-8">
+        Une collection de mes plus récents projets intéressants
+      </p>
+      <div className="container px-5  mx-auto flex flex-wrap">
+        <ul className="flex flex-wrap -m-4">
+          <Projet />
+          <Projet />
+          <Projet />
+          <Projet />
+          <Projet />
+          <Projet />
+          <Projet />
+          <Projet />
+        </ul>
+      </div>
+    </section>
     )
 }
--- pages/projets/index.jsx
+++ pages/projets/index.jsx
@@ -1,10 +1,11 @@
-import AppercuProjets from "../../components/Projets/AppercuProjets";
+import ListeProjets from "../../components/Projets/ListeProjets";
+
 
 export default function index() {
   return (
     <div className="max-w-xs mx-auto md:max-w-2xl lg:max-w-4xl xl:max-w-7xl">
 
-      <AppercuProjets />
+      <ListeProjets/>
 
     </div>
   );
