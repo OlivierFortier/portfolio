fa405f555b4201ec7b8e8b498feb0ac356c00ce4
Olivier Fortier <olivier_fortier@outlook.com>
2021-02-26 23:37:01 -0500

    commencer a work sur image appercu projet

--- components/Projets/Projet.jsx
+++ components/Projets/Projet.jsx
@@ -8,6 +8,9 @@ export default function Projet() {
         <article className="cursor-pointer">
           <div className="flex  rounded-lg shadow-md transition-shadow hover:shadow-xl p-8 md:flex-row lg:flex-col xl:flex-row flex-col">
             <div className="w-52 h-52 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded text-indigo-500 flex-shrink-0">
+              
+              {/* TODO : travailler sur la taille de l'image de preview */}
+
               <Image src="/assets/img-placeholder.png" width={165} height={165} />
             </div>
             <div className="flex-grow">
