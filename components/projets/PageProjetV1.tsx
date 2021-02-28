68d2fc52db0b0a601c0e88bba419aa919b20add1
Olivier Fortier <olivier_fortier@outlook.com>
2021-02-28 15:04:11 -0500

    ebauche gestion markdown page projet

--- components/projets/PageProjetV1.tsx
+++ components/projets/PageProjetV1.tsx
@@ -1,64 +1,41 @@
 import Link from "next/link";
 import { Projet } from "../../pages/projets/[slug]";
 import Carousel from "./Carousel";
+import ReactMarkdown from "react-markdown";
 
-export default function PageProjetV1({projet} : {projet : Projet}) {
+export default function PageProjetV1({ projet }: { projet: Projet }) {
   return (
     <section className="px-4 py-14 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 ">
       <div className="mx-auto  lg:max-w-4xl">
         <div className="max-w-xl mb-10   lg:max-w-4xl md:mb-12">
-
-        {/* TODO : Gérer le rendu markdown de cette section depuis le CMS */}
+          {/* TODO : Gérer le rendu markdown de cette section depuis le CMS */}
 
           <h1 className="max-w-lg mb-6 font-sans text-4xl font-bold leading-none tracking-tight text-blue-600 sm:text-6xl ">
             {projet.titre || "Erreur : titre non défini"}
           </h1>
           <h2 className="text-base text-blue-500 md:text-lg">
-          {projet.sommaire || "Erreur : sommaire non défini"}
+            {projet.sommaire || "Erreur : sommaire non défini"}
           </h2>
         </div>
         <div className="mb-4 transition-shadow duration-300 lg:mb-6 lg:max-w-3xl lg:mx-auto">
-
-          {/* TODO : gérer les types et le carousel dynamiquement */}
-          <Carousel images={projet.images}/>
+          <Carousel images={projet.images} />
         </div>
 
-        {/* TODO : Gérer le rendu markdown de cette section depuis le CMS */}
         <h2 className="max-w-xl mt-8 mb-4 text-2xl font-semibold text-blue-600 lg:max-w-3xl lg:mx-auto">
-          Un sous-titre
+          {projet.sousTitreDetails ||
+            "Erreur : sous-titre des détails non défini"}
         </h2>
-        <p className="max-w-xl mb-10 text-base text-blue-500 lg:max-w-3xl lg:mx-auto">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud ullamco laboris aliquip ex ea.
-        </p>
+        <ReactMarkdown className="max-w-xl mb-10 text-base text-blue-500 lg:max-w-3xl lg:mx-auto">
+          {projet.details}
+        </ReactMarkdown>
 
-        {/* TODO : Gérer le rendu markdown de cette section depuis le CMS */}
         <h2 className="max-w-xl mt-8 mb-4 text-2xl font-semibold text-blue-600 lg:max-w-3xl lg:mx-auto">
-          Les technologies utilisées
+          {projet.sousTitreSectionOptionnelle}
         </h2>
-        <ul className="space-y-3 max-w-xl mb-10 text-base text-blue-500 lg:max-w-3xl lg:mx-auto">
-          <li>
-            <span className="font-semibold">React </span> - pour controler le
-            flow de l'interface utilisateur
-          </li>
-          <li>
-            <span className="font-semibold">Next</span> - pour optimiser
-            l'expérience développement et la performance
-          </li>
-          <li>
-            <span className="font-semibold">TailwindCSS</span> - pour réaliser
-            le formatage
-          </li>
-          <li>
-            <span className="font-semibold">GraphQL</span> - pour gracieusement
-            sélectionner le contenu pour mes pages
-          </li>
-          <li>
-            <span className="font-semibold">Headless CMS</span> - pour gérer et
-            créer le contenu du site
-          </li>
-        </ul>
+
+        <ReactMarkdown className="space-y-3 max-w-xl mb-10 text-base text-blue-500 lg:max-w-3xl lg:mx-auto">
+          {projet.sectionOptionnelle}
+        </ReactMarkdown>
 
         <Link href="/projets">
           <span className="flex my-6 lg:max-w-3xl lg:mx-auto">
