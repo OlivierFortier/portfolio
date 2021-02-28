7a2c15b9a5448cb73a1a0c61b9c8c9b1bf28a4b8
Olivier Fortier <olivier_fortier@outlook.com>
2021-02-28 11:14:27 -0500

    conversion de base des composants a TS

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,14 +1,12 @@
 import { useEffect } from "react";
-import Footer from "../layout/Footer"
-import Header from "../layout/Header";
+import Footer from "./Footer";
+import Header from "./Header";
 
 export default function Layout({ children }) {
-  useEffect(
-    () =>
-      (document.querySelector("html").style.backgroundColor =
-        "rgba(249, 250, 251, 1)"),
-    []
-  );
+  useEffect(() => {
+    document.querySelector("html").style.backgroundColor =
+      "rgba(249, 250, 251, 1)";
+  }, []);
 
   return (
     <div className="bg-gray-50">
