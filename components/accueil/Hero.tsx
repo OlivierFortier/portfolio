67d90b12b4fbc79778a2c30a1baea63f94035d81
Olivier Fortier <olivier_fortier@outlook.com>
2021-03-01 17:51:16 -0500

    test animations react spring

--- components/accueil/Hero.tsx
+++ components/accueil/Hero.tsx
@@ -1,10 +1,23 @@
 import Link from "next/link";
+import { useSpring, animated } from "react-spring";
 
 export default function Hero() {
+
+  const entreeGauche = useSpring({
+    from : {opacity: 0.5, marginRight: "50rem"},
+    opacity: 1, marginRight : "0"
+  })
+
+  const entreeDroite = useSpring({
+    from : {opacity: 0.5, marginLeft: "50rem"},
+    opacity: 1, marginLeft: "0"
+  })
+
   return (
     <section className="px-4 py-14 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
       <div className="flex flex-col items-center justify-between lg:flex-row">
-        <div className="lg:self-start lg:pt-8 mb-10 lg:max-w-lg lg:pr-5 lg:mb-0">
+
+        <animated.div  className="lg:self-start lg:pt-8 mb-10 lg:max-w-lg lg:pr-5 lg:mb-0" style={entreeGauche}>
           <div className="max-w-xl mb-6">
             <h1 className="max-w-lg mb-6 font-sans text-4xl font-bold tracking-tight sm:text-6xl sm:leading-none text-blue-600">
               Olivier Fortier
@@ -21,14 +34,18 @@ export default function Hero() {
               </a>
             </Link>
           </div>
-        </div>
-        <div className="relative lg:w-1/2">
+        </animated.div>
+
+
+        <animated.div  className="relative lg:w-1/2" style={entreeDroite}>
           <img
             className="object-cover w-full h-56 rounded shadow-lg sm:h-96"
             src="https://images.pexels.com/photos/927022/pexels-photo-927022.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
             alt=""
           />
-        </div>
+        </animated.div>
+
+
       </div>
     </section>
   );
--- components/accueil/Perso.tsx
+++ components/accueil/Perso.tsx
@@ -1,6 +1,17 @@
+import { useSpring, animated } from "react-spring";
+
 export default function Perso() {
+  
+  const entreeHaut = useSpring({
+    from: { transform: "translateY(20rem)", opacity: 0.5 },
+    to: { transform: "translateY(0px)", opacity: 1 },
+  });
+
   return (
-    <section className=" pb-8 px-6 sm:px-0 text-lg md:max-w-xl lg:max-w-2xl xl:max-w-4xl lg:-mt-28 mx-auto space-y-8 text-indigo-600">
+    <animated.section
+      style={entreeHaut}
+      className=" pb-8 px-6 sm:px-0 text-lg md:max-w-xl lg:max-w-2xl xl:max-w-4xl lg:-mt-28 mx-auto space-y-8 text-indigo-600"
+    >
       <p className="shadow-sm relative bg-blue-200 p-6 -mx-6 rounded-2xl ">
         J’adore travailler avec des outils modernes et efficaces. Je passe mon
         temps à jouer avec des frameworks et des librairies pour le plaisir. Ça
@@ -14,6 +25,6 @@ export default function Perso() {
         produit multimédia. C’est pourquoi je suis inscrit à L’ETS en Génie
         Logiciel.
       </p>
-    </section>
+    </animated.section>
   );
 }
