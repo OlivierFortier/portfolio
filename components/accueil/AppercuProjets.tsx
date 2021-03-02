2a407cae91f4c9474cf183ac0d70ba9a6f917b67
Olivier Fortier <olivier_fortier@outlook.com>
2021-03-01 20:50:54 -0500

    utilisation de useInView

--- components/accueil/AppercuProjets.tsx
+++ components/accueil/AppercuProjets.tsx
@@ -1,5 +1,7 @@
 import ListeProjets from "../commun/ListeProjets";
 import GrilleSvgDroit from "./GrilleSvgDroit";
+import { useInView } from "react-intersection-observer";
+import { useSpring, animated } from "react-spring";
 
 export type ImageProjet = {
   /** Hauteur de l'image */
@@ -8,25 +10,35 @@ export type ImageProjet = {
   width: number;
   /** Chemin de l'image */
   url: string;
-}
+};
 
 export type AppercuProjet = {
   /**Titre du projet */
-  titre : string;
+  titre: string;
   /**Sommaire représentant le projet */
   sommaire: string;
   /**Tableau des images du projet */
   images: ImageProjet[];
   /**Slug du projet, utilisé pour les chemins et url */
   slug: string;
-}
+};
 
 export type ListeAppercuProjets = {
   /** Tableau des projets */
   projets: AppercuProjet[];
-}
+};
 
-export default function AppercuProjets({projets} : ListeAppercuProjets) {
+export default function AppercuProjets({ projets }: ListeAppercuProjets) {
+
+  // hook pour détecter quand un élément entre dans la vue
+  const { ref, entry } = useInView({ triggerOnce: true });
+
+  // animer si l'élément est dans la vue
+  const scaleIn = useSpring({
+    from: { transform: "scale(0)", opacity: 0 },
+    to: entry?.isIntersecting && { transform: "scale(1)", opacity: 1 },
+  });
+
   return (
     <section
       id="appercuProjets"
@@ -37,18 +49,20 @@ export default function AppercuProjets({projets} : ListeAppercuProjets) {
         <span className="relative inline-block">
           {" "}
           <span className="relative">récents</span>
-          <GrilleSvgDroit/>
+          <GrilleSvgDroit />
         </span>
       </h1>
       <p className="px-4 text-base sm:text-center text-blue-500 md:text-lg mb-8">
         Une collection de mes plus récents projets intéressants
       </p>
       <div className="container px-5  mx-auto flex flex-wrap">
-        <ul className="flex flex-wrap justify-center ">
-          
+        <animated.ul
+          style={scaleIn}
+          ref={ref}
+          className="flex flex-wrap justify-center "
+        >
           <ListeProjets projets={projets} />
-          
-        </ul>
+        </animated.ul>
       </div>
     </section>
   );
