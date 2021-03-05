53e8450217dd1fe371997996d14732eb776be0b9
Olivier Fortier <olivier_fortier@outlook.com>
2021-03-04 21:41:38 -0500

    limiter a 4 pour page accueil

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,7 +40,7 @@ export default function Home({ projets }: ListeAppercuProjets) {
 export const getStaticProps: GetStaticProps = async (context) => {
   const requeteGql = gql`
     {
-      projets(orderBy: date_DESC) {
+      projets(orderBy: date_DESC first: 4) {
         titre
         sommaire
         images {
