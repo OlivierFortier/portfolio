1b7cfa178d1690a5cd2daab3d9e2e68a76612c83
Olivier Fortier <olivier_fortier@outlook.com>
2021-03-08 22:15:48 -0500

    arranger lifecycle useFrame formes

--- components/3D/Formes.tsx
+++ components/3D/Formes.tsx
@@ -1,34 +1,67 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { MeshProps, useFrame } from "react-three-fiber";
-import { Box } from "@react-three/drei";
+import { Box, Sphere as S } from "@react-three/drei";
+import { useControls } from "leva";
 
 export function Composition() {
+  const { position } = useControls({
+    position: {
+      value: [0, 0, 0],
+      min: [-20, -20, -20],
+      max: [20, 20, 20],
+      step: 0.1,
+    },
+  });
+
+  // Avec caméra perspective
+  // positions X , de -8 à 8
+  // positions Y , de 4 à -4
+  // positions Z, de -6 à 6
+
   return (
     <group>
-      <Boite position={[0,0,0]}/>
+       {/* <Cube position={position} /> */}
+       {/* <Boite position={position} scale={[1, 1, 1]} /> */}
+      <Sphere position={position} />
     </group>
   );
 }
 
-export function Boite(props) {
+export function Cube(props) {
   const mesh = useRef<MeshProps>();
 
-  useFrame(
-    () =>
-      (mesh.current.rotation.x = mesh.current.rotation.y = mesh.current.rotation.z += 0.01)
-  );
+  let refUtile = false;
+
+  // attendre jusqu'a temps que la ref soit utile et non null
+  useEffect(()=>{
+    mesh != undefined && mesh != null && (refUtile = true);
+  },[mesh])
+
+  // animer quand la ref est utile
+ refUtile && useFrame(() => {
+    mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
+  });
 
   return (
-    <Box {...props} ref={mesh} args={[1, 1, 1]}>
-      <meshBasicMaterial attach="material" color="hotpink" />
-    </Box>
+    <mesh ref={mesh} {...props}>
+      <boxBufferGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={"yellow"} />
+    </mesh>
   );
 }
 
 export function Cone(props) {
   const mesh = useRef<MeshProps>();
 
-  useFrame(() => {
+  let refUtile = false;
+
+  // attendre jusqu'a temps que la ref soit utile et non null
+  useEffect(()=>{
+    mesh != undefined && mesh != null && (refUtile = true);
+  },[mesh])
+
+  // animer quand la ref est utile
+ refUtile && useFrame(() => {
     mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
   });
 
@@ -43,10 +76,17 @@ export function Cone(props) {
 export function Torus(props) {
   const mesh = useRef<MeshProps>();
 
-  useFrame(() => {
+  let refUtile = false;
+
+  // attendre jusqu'a temps que la ref soit utile et non null
+  useEffect(()=>{
+    mesh != undefined && mesh != null && (refUtile = true);
+  },[mesh])
+
+  // animer quand la ref est utile
+ refUtile && useFrame(() => {
     mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
   });
-
   return (
     <mesh ref={mesh} {...props}>
       <torusBufferGeometry args={[1, 1, 10]} />
@@ -58,14 +98,22 @@ export function Torus(props) {
 export function Sphere(props) {
   const mesh = useRef<MeshProps>();
 
-  useFrame(() => {
+  let refUtile = false;
+
+  // attendre jusqu'a temps que la ref soit utile et non null
+  useEffect(()=>{
+    mesh != undefined && mesh != null && (refUtile = true);
+  },[mesh])
+
+  // animer quand la ref est utile
+ refUtile && useFrame(() => {
     mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
   });
 
   return (
     <mesh ref={mesh} {...props}>
-      <sphereBufferGeometry args={[3, 3, 3]} />
-      <meshStandardMaterial color="blue" />
+      <sphereBufferGeometry args={[1, 50, 50]} />
+      <meshStandardMaterial color="lightblue" />
     </mesh>
   );
 }
--- components/3D/Scene3D.tsx
+++ components/3D/Scene3D.tsx
@@ -1,10 +1,13 @@
-import dynamic from "next/dynamic";
 import { Canvas, MeshProps, useFrame } from "react-three-fiber";
-import Boite from "./Boite";
-import { Cone, Sphere, Torus, Composition  } from "./Formes";
-import { OrbitControls, Stars, } from "@react-three/drei";
+import { Cone, Sphere, Torus, Composition } from "./Formes";
+import {
+  OrbitControls,
+  Stars,
+  PerspectiveCamera,
+  OrthographicCamera,
+} from "@react-three/drei";
 import { Theme } from "../../pages/_app";
-import { useRef } from "react";
+import { useControls } from "leva";
 
 export default function Scene3D({ theme }: { theme: Theme }) {
   const NUM = 100;
@@ -12,6 +15,9 @@ export default function Scene3D({ theme }: { theme: Theme }) {
 
   return (
     <Canvas
+      colorManagement
+      // camera={{position: [0,0,10], fov: 60}}
+      shadowMap
       style={{
         height: "100%",
         width: "100%",
@@ -20,10 +26,25 @@ export default function Scene3D({ theme }: { theme: Theme }) {
         left: 0,
       }}
     >
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
+      <PerspectiveCamera makeDefault position={[0, 0, 10]} fov={60}/>
 
-      <Composition />
+      <directionalLight
+        position={[0, 10, 0]}
+        intensity={1.5}
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
+        shadow-camera-far={50}
+        shadow-camera-left={-10}
+        shadow-camera-right={10}
+        shadow-camera-top={10}
+        shadow-camera-bottom={-10}
+      />
+      <ambientLight intensity={0.3} />
+      <pointLight position={[-10, 0, -20]} />
+      <pointLight position={[0, -10, 0]} />
+
+
+     {theme === 'light' && <Composition />}
 
       {/* <Sphere  args={[1,30,30]}>
         <meshBasicMaterial attach="material" color="hotpink" />
